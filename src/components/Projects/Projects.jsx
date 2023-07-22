25c23cfeae7bf9ef6b8a090b8b9b6fe87d34e495
Thanh Le <thanhwaslike@gmail.com>
2023-07-21 19:09:21 -0700

    Created first slide for carousel in projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,8 +3,35 @@ import './Projects.css'
 
 const Projects = () => {
   return (
-    <div className='projects-section-container' id='projects'>
-      Projects
+    <div className='projects-container' id='projects'>
+      <div className='projects-section'>
+        <div className='projects-cards-container'>
+          <div className='projects-cards'>
+            <div className='projects-image-content'>
+              <span className='overlay'>
+                <img src=".\public\img\gpt3-img.png" alt="" className='card-img' />
+              </span>
+
+              <div className='projects-cards-image'>
+                {/**/}
+              </div>
+            </div>
+
+            <div className='projects-cards-content'>
+              <h2 className='name'>
+                GPT-3 Project
+              </h2>
+              <p className="description">A fake responsive website about GPT-3 that is supposed to simulate the potentials of AI and request backers to develop it.</p>
+
+              <div className='buttons'>
+                <button className='button-1'>View Code</button>
+                <button className='button-2'>Live Demo</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
