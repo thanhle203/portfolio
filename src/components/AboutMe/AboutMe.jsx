2ae8168eebf6aef03e73c3b8d746c7438a104275
Thanh Le <thanhwaslike@gmail.com>
2023-08-28 00:11:52 -0700

    Started box geometry 3d model for about me section

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './AboutMe.css'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 const AboutMe = () => {
   return (
@@ -7,13 +9,18 @@ const AboutMe = () => {
       <div className='aboutme-section'>
         
         <div className='aboutme-left'>
-          3d model
+          <Canvas>
+            <OrbitControls />
+            <mesh>
+              <boxGeometry args={[2,2,2]} />
+            </mesh>
+          </Canvas>
         </div>
 
         <div className='aboutme-right'>
           <h2>I am a junior web developer aspiring to become a full stack developer. 
-            With a strong foundation in frontend languages such as HTML, CSS, JavaScript, and React.js, as well as some backend languages like Python and Java, I aim to create dynamic and user-friendly online experiences.
-            I'm familiar with some essential web development toolkits like MongoDB, SQL, Git, and other toolkits such as scikit-learn, NumPy, and Pandas for web-scraping.
+            With a <strong>strong</strong> foundation in frontend languages such as HTML, CSS, JavaScript, and React.js, as well as some backend languages like Python, SQL, and Java, I aim to create dynamic and user-friendly online experiences.
+            I'm familiar with some essential web development tools like MySQL, Git, and other toolkits such as scikit-learn, NumPy, and Pandas for web-scraping.
             Eager to gain real-world experience, I'm a fast learner who thrives in dynamic environments and embraces new technologies. 
             My goal is to grow as a programmer, collaborate effectively within teams, and build remarkable web applications that make a positive impact. </h2>
         </div>
